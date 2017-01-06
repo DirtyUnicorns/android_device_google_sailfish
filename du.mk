7c3fc8ed670bb69537f388c9a00d733cc5263016
David Kataja Jr <krash86.7@gmail.com>
2017-01-06 10:39:48 -0500

    fingerprint NMF26V
    Change-Id: I98ef7bd34715f67aaa484fda8effa10badcabd4f

--- du.mk
+++ du.mk
@@ -31,5 +31,5 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    BUILD_FINGERPRINT=google/sailfish/sailfish:7.1.1/NMF26Q/3537182:user/release-keys \
-    PRIVATE_BUILD_DESC="sailfish-user 7.1.1 NMF26Q 3537182 release-keys"
+    BUILD_FINGERPRINT=google/sailfish/sailfish:7.1.1/NMF26V/3562348:user/release-keys \
+    PRIVATE_BUILD_DESC="sailfish-user 7.1.1 NMF26V 3562348 release-keys"

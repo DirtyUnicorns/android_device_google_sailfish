b6054f791226a40e21ab74028e8b838d3a846d77
David Kataja Jr <krash86.7@gmail.com>
2017-03-06 20:35:26 -0500

    NOF27C - Sailfish
    Change-Id: I391e724578caa9c02b8d555d424ddf228cc490d5

--- du.mk
+++ du.mk
@@ -31,5 +31,5 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    BUILD_FINGERPRINT=google/sailfish/sailfish:7.1.1/NOF26V/3636322:user/release-keys \
-    PRIVATE_BUILD_DESC="sailfish-user 7.1.1 NOF26V 3636322 release-keys"
+    BUILD_FINGERPRINT=google/sailfish/sailfish:7.1.1/NOF27C/3687810:user/release-keys \
+    PRIVATE_BUILD_DESC="sailfish-user 7.1.1 NOF27C 3687810 release-keys"
